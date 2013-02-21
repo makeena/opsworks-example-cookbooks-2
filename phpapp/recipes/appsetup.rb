235839cb44036d509ef0eecae0825d7256082180
Myke Hines <myke@webhines.com>
2013-02-21 09:50:43 -0800

    Add perms fix

--- phpapp/recipes/appsetup.rb
+++ phpapp/recipes/appsetup.rb
@@ -7,6 +7,7 @@ node[:deploy].each do |app_name, deploy|
     code <<-EOH
     curl -s https://getcomposer.org/installer | php
     php composer.phar install
+    sudo chown -R www-data app/logs app/cache/
     EOH
   end
 end
