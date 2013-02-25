c7247bc139f8e1eeb7d19398965d37e0e711dfcb
Myke Hines <myke@webhines.com>
2013-02-25 09:01:27 -0800

    Add web/uploads

--- phpapp/recipes/appsetup.rb
+++ phpapp/recipes/appsetup.rb
@@ -6,6 +6,7 @@ node[:deploy].each do |app_name, deploy|
     cwd "#{deploy[:deploy_to]}/current"
     code <<-EOH
     sudo chown -R www-data cache
+    sudo chown -R www-data web/uploads
     EOH
     only_if "test -d #{cwd}/cache"
   end
